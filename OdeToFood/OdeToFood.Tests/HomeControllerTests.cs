4fa3d70b78827f8b437d8318e7fa9e1997a38119
Dave820SA <dwdavidson@live.com>
2017-06-06 15:12:25 -0500

    Added A test

---
+++ OdeToFood/OdeToFood.Tests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeToFood.Controllers;
+
+namespace OdeToFood.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Puts_Message_In_ViewBag()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+            Assert.IsNotNull(result.ViewBag.Message);
+        }
+    }
+}
--- OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OdeToFood.Models;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ViewResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-            return View();
+            var model = new RestaurantReview()
+            {
+                Name = "Big Daddies",
+                Rating = 9
+            };
+
+
+            return View(model);
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Location = "Texas is best";
 
             return View();
         }
